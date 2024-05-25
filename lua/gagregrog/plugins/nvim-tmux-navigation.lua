649c79c242fc55a24cfcc0d18a5dacbab0fe6b02
Gary Gregory Rogers <gagregrog@gmail.com>
2024-05-24 19:41:26 -0700

    feat: consistent tmux/vim nav

--- lua/gagregrog/plugins/nvim-tmux-navigation.lua
+++ lua/gagregrog/plugins/nvim-tmux-navigation.lua
@@ -7,11 +7,15 @@ return {
       disable_when_zoomed = true
     })
 
-    keymap.set('n', "<leader>nf", vtm.NvimTmuxNavigateUp, { desc = "Navigate up from current split" }) 
-    keymap.set('n', "<leader>ns", vtm.NvimTmuxNavigateDown, { desc = "Navigate down from current split" }) 
+    keymap.set('n', "<leader>nf", vtm.NvimTmuxNavigateUp, { desc = "Navigate up from current split" })
+    keymap.set('n', "<C-k>", vtm.NvimTmuxNavigateUp, { desc = "Navigate up from current split" })
+    keymap.set('n', "<leader>ns", vtm.NvimTmuxNavigateDown, { desc = "Navigate down from current split" })
+    keymap.set('n', "<C-j>", vtm.NvimTmuxNavigateDown, { desc = "Navigate down from current split" })
     keymap.set('n', "<leader>nr", vtm.NvimTmuxNavigateLeft, { desc = "Navigate left from current split" })
-    keymap.set('n', "<leader>nt", vtm.NvimTmuxNavigateRight, { desc = "Navigate right from current split" }) 
-    keymap.set('n', "<leader>np", vtm.NvimTmuxNavigateLastActive, { desc = "Navigate to previous split" }) 
-    keymap.set('n', "<leader>nn", vtm.NvimTmuxNavigateNext, { desc = "Navigate to next split" }) 
+    keymap.set('n', "<C-h>", vtm.NvimTmuxNavigateLeft, { desc = "Navigate left from current split" })
+    keymap.set('n', "<leader>nt", vtm.NvimTmuxNavigateRight, { desc = "Navigate right from current split" })
+    keymap.set('n', "<C-l>", vtm.NvimTmuxNavigateRight, { desc = "Navigate right from current split" })
+    keymap.set('n', "<leader>np", vtm.NvimTmuxNavigateLastActive, { desc = "Navigate to previous split" })
+    keymap.set('n', "<leader>nn", vtm.NvimTmuxNavigateNext, { desc = "Navigate to next split" })
   end,
 }
