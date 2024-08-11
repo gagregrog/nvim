e17741171fcc30d135105fbb17cec34826b00089
Gary Gregory Rogers <gagregrog@gmail.com>
2024-08-10 18:51:18 -0700

    feat: oil file explorer!

--- lua/gagregrog/plugins/alpha.lua
+++ lua/gagregrog/plugins/alpha.lua
@@ -20,6 +20,7 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+      dashboard.button("-", "  > Open Oil File Explorer", "<cmd>Oil<CR>"),
       dashboard.button("<space> ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
       dashboard.button("<space> ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("<space> fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
