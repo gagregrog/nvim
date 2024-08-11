757ad01ccf92841d9635153d64aebf2e8d7a8d0e
Gary Gregory Rogers <gagregrog@gmail.com>
2024-08-10 22:36:32 -0700

    feat: linting and formatting

--- lua/gagregrog/plugins/lsp/mason.lua
+++ lua/gagregrog/plugins/lsp/mason.lua
@@ -1,52 +1,54 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    -- TODO: enable additional servers
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        -- "bashls",
-        -- "clangd",
-        -- "cmake",
-        "cssls",
-        -- "dockerls",
-        "emmet_ls",
-        -- "gopls",
-        "graphql",
-        "html",
-        "lua_ls",
-        "pyright",
-        -- "sqlls",
-        "tsserver",
-        -- "vimls",
-      },
-    })
+		-- TODO: enable additional servers
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				-- "bashls",
+				-- "clangd",
+				-- "cmake",
+				"cssls",
+				-- "dockerls",
+				"emmet_ls",
+				-- "gopls",
+				"graphql",
+				"html",
+				"lua_ls",
+				"pyright",
+				-- "sqlls",
+				"tsserver",
+				-- "vimls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+			},
+		})
+	end,
 }
