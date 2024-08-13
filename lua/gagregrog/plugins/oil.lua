18d8a006d52d9e92fc71b5cac12cbd940a96e0f0
Gary Gregory Rogers <gagregrog@gmail.com>
2024-08-12 22:42:17 -0700

    fix: better oil auto preview

--- lua/gagregrog/plugins/oil.lua
+++ lua/gagregrog/plugins/oil.lua
@@ -1,30 +1,31 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local oil = require("oil")
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local oil = require("oil")
 
-    oil.setup({
-      columns = {
-        "icon",
-        "size",
-        -- "mtime", -- if you want timestamps when last modified
-      },
-      constrain_cursor = "name",
-      view_options = {
-        show_hidden = true,
-        is_always_hidden = function(name, bufnr)
-          return name == ".."
-        end,
-      },
-    })
-    
-    vim.keymap.set('n', '-', function()
-      oil.open()
-
-      require('oil.util').run_after_load(0, function()
-        oil.open_preview()
-      end)
-    end, { desc = 'Open oil file buffer' })
-  end,
+		oil.setup({
+			columns = {
+				"icon",
+				"size",
+				-- "mtime", -- if you want timestamps when last modified
+			},
+			constrain_cursor = "name",
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name)
+					return name == ".."
+				end,
+			},
+		})
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "OilEnter",
+			callback = vim.schedule_wrap(function(args)
+				if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+					oil.open_preview()
+				end
+			end),
+		})
+	end,
 }
