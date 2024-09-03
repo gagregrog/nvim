6cdb3eec011307f6dbed5678d2a4a6b158f448cd
Gary Gregory Rogers <gagregrog@gmail.com>
2024-09-02 18:47:26 -0700

    feat: auto source auto session

--- lua/gagregrog/plugins/auto-session.lua
+++ lua/gagregrog/plugins/auto-session.lua
@@ -1,20 +1,21 @@
 return {
-  "rmagatti/auto-session",
-  config = function()
-    local auto_session = require("auto-session")
-    auto_session.setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = {
-        "~/",
-        "~/Downloads",
-        "~/Documents",
-        "~/Desktop",
-      },
-    })
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session = require("auto-session")
+		auto_session.setup({
+			auto_restore_enabled = false,
+			auto_session_suppress_dirs = {
+				"~/",
+				"~/Downloads",
+				"~/Documents",
+				"~/Desktop",
+			},
+		})
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
-    keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
-  end,
+		-- the extra <CR> invokes the command immediately, rather than warning you about the replaced buffers
+		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR><CR>", { desc = "Restore session for cwd" })
+		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+	end,
 }
