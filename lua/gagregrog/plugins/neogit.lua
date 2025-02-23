5bf02495acc84748dc7cb4584fcdd4e4ab4857d5
Gary Gregory Rogers <gagregrog@gmail.com>
2025-02-22 20:21:21 -0800

    feat: vertical diff view by default

--- lua/gagregrog/plugins/neogit.lua
+++ lua/gagregrog/plugins/neogit.lua
@@ -7,6 +7,15 @@ return {
 	},
 	config = function()
 		require("neogit").setup({})
+		require("diffview").setup({
+			view = {
+				-- For more info, see |diffview-config-view.x.layout|.
+				default = {
+					-- Config for changed files, and staged files in diff views.
+					layout = "diff2_vertical",
+				},
+			},
+		})
 		vim.keymap.set("n", "<leader>pr", ":DiffviewOpen origin/main..HEAD<CR>", { noremap = true, silent = true })
 	end,
 }
