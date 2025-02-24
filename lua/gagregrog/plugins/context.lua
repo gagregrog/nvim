40a374c326101c4ff7e9ca9d8ea4ff895b176c73
Gary Gregory Rogers <gagregrog@gmail.com>
2025-02-23 17:02:44 -0800

    fix: less context lines

--- lua/gagregrog/plugins/context.lua
+++ lua/gagregrog/plugins/context.lua
@@ -1,6 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter-context",
 	config = function()
+		require("treesitter-context").setup({
+			max_lines = 4,
+			multiline_threshold = 10,
+		})
 		vim.keymap.set("n", "<leader>cc", function()
 			require("treesitter-context").go_to_context(vim.v.count1)
 		end, { silent = true, desc = "Jump to context" })
