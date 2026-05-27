29a8eb7e32d88abbec4def040ed1b734e02c5f73
Gary Gregory Rogers <gagregrog@gmail.com>
2026-05-26 22:05:41 -0700

    feat: tab binds

--- lua/gagregrog/core/keymaps.lua
+++ lua/gagregrog/core/keymaps.lua
@@ -18,6 +18,18 @@ keymap.nmap("<leader>se", "<C-w>=", "Make splits equal size")
 keymap.nmap("<leader>sc", "<cmd>close<CR>", "Close current split")
 keymap.nmap("<leader>sx", "<cmd>close<CR>", "Close current split")
 
+-- tab management
+keymap.nmap("<leader>tt", "<cmd>tabnew<CR>", "New tab")
+keymap.nmap("<leader>tT", "<cmd>tabnew %<CR>", "New tab with current file")
+keymap.nmap("<leader>tx", "<cmd>tabclose<CR>", "Close current tab")
+keymap.nmap("<leader>tX", "<cmd>tabonly<CR>", "Close all other tabs")
+keymap.nmap("<leader>tn", "<cmd>tabnext<CR>", "Next tab")
+keymap.nmap("<leader>tp", "<cmd>tabprevious<CR>", "Previous tab")
+keymap.nmap("<leader>tN", "<cmd>tablast<CR>", "Go to last tab")
+keymap.nmap("<leader>tP", "<cmd>tabfirst<CR>", "Go to first tab")
+keymap.nmap("<leader>th", "<cmd>-tabmove<CR>", "Move tab left")
+keymap.nmap("<leader>tl", "<cmd>+tabmove<CR>", "Move tab right")
+
 -- increment/decrement
 keymap.nmap("<A-a>", "<C-a>", "Increment number under cursor")
 keymap.nmap("<A-r>", "<C-x>", "Decrement number under cursor")
